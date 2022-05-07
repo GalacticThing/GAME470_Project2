af03c4aaa29605b9049d48cc76779d4db46c8e18
ZacharyLawrence13 <pookaguy01@yahoo.com>
2022-05-07 02:55:45 -0400

    Created Pausing, going back to main menu doesnt work
    Gong bac to main menu doesnt work. Will get it fixed tomorrow morning

--- GAME_470_Project2/Assets/Scripts/Pause.cs
+++ GAME_470_Project2/Assets/Scripts/Pause.cs
@@ -1,40 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
     public bool isPaused;
-    public GameObject Shuffle1Button;
+    public GameObject pauseCanvas;
+    public Slider volumeSlider;
+    public AudioSource music;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        pauseCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (!isPaused && (Input.GetKeyDown(KeyCode.P)))
         {
             isPaused = true;
         }
-        if (Input.GetKeyDown("o"))
+        if (isPaused && (Input.GetKeyDown(KeyCode.O)))
         {
             isPaused = false;
         }
 
         if(isPaused == true)
         {
-            Time.timeScale = 0;
-            Shuffle1Button.SetActive(false);
+            Time.timeScale = 0.01f;
+            pauseCanvas.SetActive(true);
+            music.volume = volumeSlider.value;
         }
         else
         {
             Time.timeScale = 1;
-            Shuffle1Button.SetActive(true);
+            pauseCanvas.SetActive(false);
         }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "StartMenu");
+    }
+
+    public void clickQuit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
     }
 
     public IEnumerator DeactivateButtons()
